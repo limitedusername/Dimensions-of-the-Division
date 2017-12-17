2eec0ac68475604dd7e75b4044ea260f2e9d46cd
HyeJune Suh <hyejune.suh@gmail.com>
2017-12-17 13:17:07 +0900

    initial

---
+++ SLAVE1_servo/SLAVE1_servo.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+
+#include <Servo.h>
+
+// 자신의 슬레이브 주소를 설정해 줍니다.(슬레이브 주소에 맞게 수정해야 합니다.)
+#define SLAVE 2
+Servo myservo; 
+int pos = 0;   
+// 카운터
+byte count = 0; 
+char temp;
+
+void setup() {
+  // Wire 라이브러리 초기화
+  // 슬레이브로 참여하기 위해서는 주소를 지정해야 한다.
+  Wire.begin(SLAVE);
+  Wire.onReceive(receiveFromMaster);
+  // 마스터의 데이터 전송 요구가 있을 때 처리할 함수 등록
+  Wire.onRequest(sendToMaster);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop () {
+  // 요청이 들어오면 13번 LED를 0.5초간 켜줍니다.
+  if (temp == 'o') { 
+    play();
+    circle();
+  }
+}
+
+void receiveFromMaster(int bytes) {
+  char ch[2];
+  for (int i = 0 ; i < bytes ; i++) {
+    // 수신 버퍼 읽기
+    ch[i] = Wire.read(); 
+  }
+  temp = ch[0];
+}
+
+void circle() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void play() { 
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  temp = 0;
+}
+
+void sendToMaster() {
+  // 자신의 슬레이브 주소를 담은 메세지를 마스터에게 보냅니다. 슬레이브 주소에 맞게 수정해야 합니다.
+  Wire.write("2 Arduino ");
+}
